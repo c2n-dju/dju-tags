0a3c27117955ac1a34139b59f7d9044d99fc3f0b
Fabrice Pardo <fabrice.pardo@u-psud.fr>
2020-05-02 19:33:51 +0200

    Remove unused six import

--- djutags/templatetags/dju_tags.py
+++ djutags/templatetags/dju_tags.py
@@ -10,7 +10,6 @@ from django import template
 from django.contrib.sites.models import Site
 from django.urls import reverse, NoReverseMatch
 from django.utils.encoding import force_text
-from django.utils.six.moves.urllib.parse import unquote
 from django.utils.translation import get_language, ugettext
 
 #from menus.menu_pool import menu_pool
